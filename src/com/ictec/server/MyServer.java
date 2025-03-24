06925f4c4a23e8bd395d2dcfaf8a4d51c99b95c6
Nepul Dhanujaya Seneviratne <116420215+Nepul1234@users.noreply.github.com>
2025-03-24 21:37:05 +0530

    Edit MyServer.java file

--- src/com/ictec/server/MyServer.java
+++ src/com/ictec/server/MyServer.java
@@ -1,7 +1,26 @@
 package com.ictec.server;
 
+import com.ictec.cal.MyCalcInter;
+import com.ictec.cal.MyCalculator;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class MyServer {
     public static void main(String[] args) {
         //1. Create a registry
+        try {
+            Registry registry = LocateRegistry.createRegistry(55321);
+
+            //2. Binding the remote object
+            MyCalcInter mycal = new MyCalculator();
+            registry.rebind("MyCalServer", mycal);
+
+            System.out.println("MyCalServer is ready...");
+
+        } catch (RemoteException e) {
+            System.out.println("Error in registry creation" + e.getMessage());
+        }
     }
 }
